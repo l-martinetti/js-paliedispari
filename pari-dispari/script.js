94db7b37b37528da73e5098357eed01cb0929fe6
l-martinetti <luca.martinetti@outlook.it>
2024-11-19 19:51:13 +0100

    feat: add pari-dispari excercise complete

---
+++ pari-dispari/script.js
@@ -0,0 +1,39 @@
+//Variabili
+const playOddEven = prompt("Scegli PARI o DISPARI");
+const playerChoice = playOddEven.toUpperCase();
+const playerNumber = prompt("Scegli un numero da 1 a 5");
+const pcNumber = getRandomNumber();
+
+let sum = playerNumber + pcNumber;
+let message;
+
+
+
+//Logica
+if (isEven(sum) == true && playerChoice === "PARI") {
+    message = "Hai vinto";
+} else if (isEven(sum) == false && playerChoice === "DISPARI") {
+    message = "Hai vinto";
+} else {
+    message = "Ha vinto il pc";
+}
+
+//Log
+
+console.log(playOddEven);
+console.log(sum);
+console.log(message);
+
+
+//Funzioni
+function getRandomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+function isEven(num) {
+    let evenNumber = false;
+    if (!(num % 2)) {
+        evenNumber = true;
+    } 
+    return evenNumber
+}
